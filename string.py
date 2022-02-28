5c462bcab1ec5382c23e77164340530f06b29e9f
K.Sathish <74056043+Sathish-05@users.noreply.github.com>
2022-02-28 13:16:07 +0530

    Create string.py

---
+++ string.py
@@ -0,0 +1,15 @@
+n= "My captian"
+#Slicing
+print(n[2:5])
+print(n[2:])
+print(n[:5])
+print(n[-2])
+#Upper Case
+print(n.upper())
+#Upper Case
+print(n.lower())
+#Replace String
+print(n.replace("m", "M"))
+#String Concatenation
+cap="App"
+print(n+" "+cap)
