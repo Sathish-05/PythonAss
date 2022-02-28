07c501da08085431c5ef673a01b35d8d1fe12a11
K.Sathish <74056043+Sathish-05@users.noreply.github.com>
2022-02-28 13:30:27 +0530

    Create KeywordArguments.py

---
+++ KeywordArguments.py
@@ -0,0 +1,6 @@
+def my_function(name, Age, place):
+  print("I am "+name+".My Age is "+Age+".I am from "+place)
+name=input("Enter your Name:")
+Age=input("Enter your Age:")
+place=input("Enter your place:")
+my_function(name, Age, place)
